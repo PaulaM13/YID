ba51992f9cfd01119d738da3a05dc77d04e6662b
PaulaM13 <109590965+PaulaM13@users.noreply.github.com>
2022-09-29 19:06:21 +0200

    Update invitados.js

--- js/invitados.js
+++ js/invitados.js
@@ -1,1 +1,27 @@
-# YID
+async function fetchJSON4() {
+  const response = await fetch("http://localhost:8080/invitados");
+  const jsonResponse = await response.json();
+  return jsonResponse;
+}
+fetchJSON4().then((json) => {
+  console.log(json); // objeto al completo [{}, {}, {}]
+  for (const element of json) {
+    console.log(element); //{}
+    //console.log(`${element.idUsuario}`);
+    const tableTbody4 = document.getElementById("tableTbody4");
+    const tableRow = (document.createElement("tr").innerHTML = `
+      
+    
+            <tr>
+                    <th>${element.idInvitado}</th>
+                    <th>${element.nombreInvitado}</th>
+                    <th>${element.apellidoInvitado}</th>
+                    <th>${element.correoElectronico}</th>
+                    <th>${element.cantInvitacion}</th>
+                    <th>${element.nroMenu}</th>
+                    <th>${element.necesidadEspecial}</th>
+                        </tr>
+          `);
+    tableTbody4.innerHTML += tableRow;
+  }
+});
